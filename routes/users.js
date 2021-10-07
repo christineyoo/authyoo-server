c3fa01bc0f0b0249d936472de4964ff54fb1d8ba
Christine Yoo <christinecho.s@gmail.com>
2021-10-06 21:32:22 -0700

    implement passport auth and login logic

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const passport = require('passport')
 
 //User model
 const User = require('../models/User')
@@ -76,6 +77,22 @@ router.post('/register', (req, res) => {
         })
         .catch()
   }
+})
+
+//Login Handle
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next)
+})
+
+//Logout Handle
+router.get('/logout', (req, res) => {
+  req.logout() //logout is from passport
+  req.flash('success_msg', 'You are logged out')
+  res.redirect('/users/login')
 })
 
 module.exports = router
